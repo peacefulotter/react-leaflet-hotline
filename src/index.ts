10d3797c9842a1d8eb6ccc8a5edd933a0269c018
PeacefulOtter <zankro67@gmail.com>
2022-06-24 16:26:06 +0200

    Added useCustomHotline to the exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 
 export { default as Hotline } from './Hotline'
 export { default as useHotline } from './useHotline'
+export { default as useCustomHotline } from './useCustomHotline'
+
 export { default as Renderer } from './renderers/Renderer'
 export { default as HotPolyline } from './core/HotPolyline'
 
