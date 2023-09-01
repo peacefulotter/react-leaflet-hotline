ba230f5b089e93ad437ed44475e40ecae4c874fa
PeacefulOtter <zankro67@gmail.com>
2023-09-01 21:49:49 +0200

    More work on docs to work with leaflet and react-leaflet-hotline, running build-fixup at the end of the build phase

--- nextra-docs-template/components/Demo.tsx
+++ nextra-docs-template/components/Demo.tsx
@@ -1,12 +1,16 @@
-import { Hotline } from 'react-leaflet-hotline';
-import MapWrapper from './map/MapWrapper';
+import dynamic from "next/dynamic";
 
 import { data, options } from '../assets/constants';
+
+const MapWrapper = dynamic(() => import("./map/MapWrapper"), {
+	ssr: false
+});
+import { Hotline } from 'react-leaflet-hotline/lib';
 
 export default function Demo() {
 	return (
 		<MapWrapper>
-			<Hotline 
+			<Hotline
 				data={data} 
 				getLat={t => t.lat} 
 				getLng={t => t.lng} 
